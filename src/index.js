b04bf23abee579fec1b7105c6994418a76d53e5a
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-11-11 14:15:47 +0100

    add the option to create new task

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ import { compareAsc, format } from "date-fns";
 
 let task = createTask();
 task.setTitle("sleep");
-task.setDueDate(format(new Date(2024, 10, 9), "MM/dd/yy"));
+task.setDueDate(format(new Date(2024, 10, 11), "yyyy-MM-dd"));
 
 let task1 = createTask();
 task1.setTitle("sleep1");
-task1.setDueDate(format(new Date(2024, 10, 8), "MM/dd/yy"));
+task1.setDueDate(format(new Date(2024, 10, 11), "yyyy-MM-dd"));
 
 let task2 = createTask();
 task2.setTitle("sleep2");
-task2.setDueDate(format(new Date(2024, 10, 8), "MM/dd/yy"));
+task2.setDueDate(format(new Date(2024, 10, 8), "yyyy-MM-dd"));
 
 let task3 = createTask();
-task3.setTitle(format(new Date(2024, 10, 8), "MM/dd/yy"));
-task3.setDueDate("11/05/24");
+task3.setTitle("sleep more");
+task3.setDueDate(format(new Date(2024, 10, 23), "yyyy-MM-dd"));
 
 let task4 = createTask();
 task4.setTitle("sleep4");
-task4.setDueDate(format(new Date(2024, 10, 8), "MM/dd/yy"));
+task4.setDueDate(format(new Date(2024, 10, 15), "yyyy-MM-dd"));
 
 let newProject = project();
 newProject.setProjectName("lazyness");
@@ -29,6 +29,9 @@ newProject.addTask(task1);
 newProject.addTask(task2);
 newProject.addTask(task3);
 newProject.addTask(task4);
+
+let secondProject = project();
+newProject.setProjectName("fitness");
 
 newProject.getTasks().forEach(task => console.log(task.info()));
 console.log(Date());
@@ -50,73 +53,163 @@ console.log(format(date,"MM/dd/yy")); */
 function dispalyController() {
     const menu = document.querySelector(".sideBarMenu");
     const content = document.querySelector(".content");
-    let allProjects = projects()
+    const dialog = document.querySelector("dialog");
+    let allProjects = projects();
     allProjects.addProjects(newProject);
     menu.addEventListener("click", (e) => {
         if (e.target.className === "today") {
-            todaytasks(allProjects.getProjects()).forEach(task => console.log(task.info()));
-            //console.log(allProjects.getProjects()[0].getTasks()[0].info());
-            return;
-        } if (e.target.className === "upcomming") {
-            upcommingTasks(allProjects.getProjects()).forEach(task => console.log(task.info()));
-            //console.log(allProjects.getProjects()[0].getTasks()[0].info());
+            displayTodayTasks(content, allProjects);
             return;
         }
         if (e.target.className === "upcomming") {
-            upcommingTasks(allProjects.getProjects()).forEach(task => console.log(task.info()));
+            displayUpcommingTasks(content, allProjects);
             //console.log(allProjects.getProjects()[0].getTasks()[0].info());
             return;
-        }  if (e.target.className === "logbook") {
-            logbook(allProjects.getProjects()).forEach(task => console.log(task.info()));
+        } if (e.target.className === "logbook") {
+            getDueTasks(allProjects.getProjects()).forEach(task => console.log(task.info()));
+            displayDueTasks(content, allProjects);
             //console.log(allProjects.getProjects()[0].getTasks()[0].info());
             return;
-        } 
+        }
+    });
+    content.addEventListener("click", (e) => {
+        console.log(e.target.className);
+        e.target.preve
+        if (e.target.className === "open") {
+            let d = "hello";
+            dialog.showModal();
+        } else if (e.target.className.includes("close")) {
+            dialog.close();
+        } else if (e.target.className.includes("add")) {
+            e.preventDefault();
+            addToPreject(allProjects, "lazyness", addNewTask());
+        }
     })
 
 }
-function todaytasks(projects) {
-    let todayTasksList = [];
-    let todayDate = format(Date(), "MM/dd/yy");
+function getTodaytasks(projects) {
+    let taskList = [];
+    let todayDate = format(Date(), "yyyy-MM-dd");
     projects.forEach(project => {
         const tasksForToday = project.getTasks().filter(task => task.getDueDate() === todayDate);
         console.log(tasksForToday);
-        todayTasksList.push(...tasksForToday);
+        taskList.push(...tasksForToday);
     });
-    return Array.from(todayTasksList)
+    return Array.from(taskList)
 }
 
-function upcommingTasks(projects) {
-    let todayTasksList = [];
-    let todayDate = format(Date(), "MM/dd/yy");
+function getUpcomingTasks(projects) {
+    let taskList = [];
+    let todayDate = format(Date(), "yyyy-MM-dd");
     projects.forEach(project => {
         const tasksForToday = project.getTasks().filter(task => task.getDueDate() > todayDate);
         console.log(tasksForToday);
-        todayTasksList.push(...tasksForToday);
+        taskList.push(...tasksForToday);
     });
-    return Array.from(todayTasksList)
+    return Array.from(taskList)
 }
 
-function logbook(projects) {
-    let todayTasksList = [];
-    let todayDate = format(Date(), "MM/dd/yy");
+function getDueTasks(projects) {
+    let taskList = [];
+    let todayDate = format(Date(), "yyyy-MM-dd");
     projects.forEach(project => {
         const tasksForToday = project.getTasks().filter(task => task.getDueDate() < todayDate);
         console.log(tasksForToday);
-        todayTasksList.push(...tasksForToday);function upcommingTasks(projects) {
-            let todayTasksList = [];
-            let todayDate = format(Date(), "MM/dd/yy");
-            projects.forEach(project => {
-                const tasksForToday = project.getTasks().filter(task => task.getDueDate() > todayDate);
-                console.log(tasksForToday);
-                todayTasksList.push(...tasksForToday);
-            });
-            return Array.from(todayTasksList)
-        }
+        taskList.push(...tasksForToday);
     });
-    return Array.from(todayTasksList)
+    return Array.from(taskList)
 }
 function eventController(inputDom, outputDom) {
     inputDom.addEvent
 }
 
+function displayTodayTasks(content, projects) {
+    getTodaytasks(projects.getProjects()).forEach(task => {
+        const div = document.createElement("div");
+        const taskTitle = document.createElement("p");
+        const taskDate = document.createElement("p");
+        const taskDescription = document.createElement("p");
+        const taskPriority = document.createElement("p");
+
+        taskTitle.textContent = task.getTitle();
+        taskDate.textContent = task.getDueDate();
+        taskDescription.textContent = task.getDescription();
+        taskPriority.textContent = task.getPriority();
+
+        div.className = "card";
+        div.appendChild(taskTitle);
+        div.appendChild(taskDate);
+        div.appendChild(taskDescription);
+        div.appendChild(taskPriority);
+        content.appendChild(div);
+    }
+    );
+}
+
+function displayUpcommingTasks(content, projects) {
+    getUpcomingTasks(projects.getProjects()).forEach(task => {
+        const div = document.createElement("div");
+        const taskTitle = document.createElement("p");
+        const taskDate = document.createElement("p");
+        const taskDescription = document.createElement("p");
+        const taskPriority = document.createElement("p");
+
+        taskTitle.textContent = task.getTitle();
+        taskDate.textContent = task.getDueDate();
+        taskDescription.textContent = task.getDescription();
+        taskPriority.textContent = task.getPriority();
+
+        div.className = "card";
+        div.appendChild(taskTitle);
+        div.appendChild(taskDate);
+        div.appendChild(taskDescription);
+        div.appendChild(taskPriority);
+        content.appendChild(div);
+    }
+    );
+}
+
+function displayDueTasks(content, projects) {
+    getDueTasks(projects.getProjects()).forEach(task => {
+        const div = document.createElement("div");
+        const taskTitle = document.createElement("p");
+        const taskDate = document.createElement("p");
+        const taskDescription = document.createElement("p");
+        const taskPriority = document.createElement("p");
+
+        taskTitle.textContent = task.getTitle();
+        taskDate.textContent = task.getDueDate();
+        taskDescription.textContent = task.getDescription();
+        taskPriority.textContent = task.getPriority();
+
+        div.className = "card";
+        div.appendChild(taskTitle);
+        div.appendChild(taskDate);
+        div.appendChild(taskDescription);
+        div.appendChild(taskPriority);
+        content.appendChild(div);
+    }
+    );
+}
+function addNewTask() {
+    const title = document.querySelector("#title");
+    const dueDate = document.querySelector("#dueDate");
+    const description = document.querySelector("#description");
+    const priority = document.querySelector("#priority");
+    let task = createTask();
+    task.setTitle(title.value);
+    task.setDueDate(dueDate.value);
+    task.setDescription(description.value);
+    task.setPriority(priority.value);
+
+    return task;
+}
+
+function addToPreject(projects, projectName, task) {
+    projects.getProjects().forEach(project => {
+        if (project.getProjectName() === projectName) {
+            project.addTask(task)
+        }
+    })
+}
 dispalyController();
